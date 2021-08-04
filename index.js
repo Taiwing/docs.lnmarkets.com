5719175528d94fbc959ef0a990eca40164308d58
Victor Afanassieff <victor@afanassieff.com>
2021-08-04 20:08:49 +0200

    feat: generate api doc from json file now

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-"use strict";
+const fs = require('fs/promises')
+const path = require('path')
+const converter = require('widdershins')
+const SwaggerParser = require('@apidevtools/swagger-parser')
 
-const fs = require("fs/promises");
-const path = require("path");
-const yaml = require("yaml");
-const converter = require("widdershins");
-const SwaggerParser = require("@apidevtools/swagger-parser");
+const apiPath = path.join(__dirname, './spec.json')
+const slateFile = path.join(__dirname, './_api.md')
 
-const apiPath = path.join(__dirname, "./spec/index.yaml");
-const slateFile = path.join(__dirname, "./_api.md");
-const JSONFile = path.join(__dirname, "./dist/openapi.json");
-
-const openApiToSlate = async () => {
+const main = async () => {
   try {
     const options = {
       codeSamples: true,
@@ -19,53 +15,26 @@ const openApiToSlate = async () => {
       omitHeader: true,
       httpsnippet: true,
       tocSummary: true,
-      user_templates: "./template",
+      user_templates: './template',
       language_tabs: [
-        { python: "Python" },
-        { "javascript--node": "Node.js" },
-        { shell: "Shell" },
+        { python: 'Python' },
+        { 'javascript--node': 'Node.js' },
+        { shell: 'Shell' },
       ],
       language_clients: [
-        { shell: "curl" },
-        { "javascript--node": "request" },
-        { python: "requests" },
+        { shell: 'curl' },
+        { 'javascript--node': 'request' },
+        { python: 'requests' },
       ],
-    };
+    }
 
-    const raw = await fs.readFile(apiPath, "utf-8");
-    const parsed = yaml.parse(raw);
-    const data = await converter.convert(parsed, options);
-    await fs.writeFile(slateFile, data);
+    const raw = await fs.readFile(apiPath, 'utf-8')
+    const parsed = JSON.parse(raw)
+    const data = await converter.convert(parsed, options)
+    await fs.writeFile(slateFile, data)
   } catch (error) {
-    return Promise.reject(error);
+    console.error(error)
   }
-};
+}
 
-const openApiToJSON = async () => {
-  try {
-    const parser = new SwaggerParser();
-    const apiSpec = await parser.validate(apiPath);
-    await fs.mkdir(path.join(__dirname, "dist")).catch((error) => {
-      if (error.code !== "EEXIST") {
-        return Promise.reject(error);
-      }
-      return Promise.resolve();
-    });
-    await fs.writeFile(JSONFile, JSON.stringify(apiSpec, null, 2));
-    return apiSpec;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
-
-const main = async () => {
-  try {
-    await openApiToJSON();
-    await openApiToSlate();
-  } catch (error) {
-    console.error(error);
-    process.exit(-1);
-  }
-};
-
-main();
+main()
