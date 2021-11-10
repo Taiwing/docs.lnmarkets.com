573a768ff5cd224da4d53947341e8023add706fb
Victor Afanassieff <victor@afanassieff.com>
2021-11-10 15:39:28 +0100

    feat: api doc with redoc

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 const fs = require('fs/promises')
 const path = require('path')
-const converter = require('widdershins')
-const SwaggerParser = require('@apidevtools/swagger-parser')
 
 const apiPath = path.join(__dirname, './spec.json')
-const slateFile = path.join(__dirname, './_api.md')
 
 const main = async () => {
   try {
-    const options = {
-      codeSamples: true,
-      resolve: true,
-      source: apiPath,
-      omitHeader: true,
-      httpsnippet: true,
-      tocSummary: true,
-      user_templates: './template',
-      language_tabs: [
-        { python: 'Python' },
-        { 'javascript--node': 'Node.js' },
-        { shell: 'Shell' },
-      ],
-      language_clients: [
-        { shell: 'curl' },
-        { 'javascript--node': 'native' },
-        { python: 'requests' },
-      ],
-    }
-
     const raw = await fs.readFile(apiPath, 'utf-8')
-    const parsed = JSON.parse(raw)
-    const data = await converter.convert(parsed, options)
-    await fs.writeFile(slateFile, data)
+    const spec = JSON.parse(raw)
+
+    spec.info.description = { $ref: './description.md' }
+
+    await fs.writeFile(apiPath, JSON.stringify(spec, null, 2))
   } catch (error) {
     console.error(error)
   }
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,21 +2,4 @@
 
 set -o errexit
 
-cp ./images/logo.png ./slate/source/images/logo.png
-
-cp ./_api.md ./slate/source/includes/_api.md
-
-cd docs
-
-for filename in *.md
-do
-    if [ $filename = 'index.html.md' ]; then
-        cp index.html.md ../slate/source
-    else
-        cp "$filename" "../slate/source/includes/_$filename"
-    fi
-done
-
-cd ../slate
-
-bundle exec middleman build --clean --build-dir ../build
+pnpm redoc-cli bundle --output ./build/index.html --title "LN Markets Api Reference" spec.json
